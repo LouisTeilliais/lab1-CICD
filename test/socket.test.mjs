7f577c8d749d147ce47d2cbd0f971a28748f7596
Louis Teilliais <louisteilliais@MacBook-Pro-de-Louis.local>
2024-01-25 15:51:03 +0100

    deploy

---
+++ test/socket.test.mjs
@@ -0,0 +1,13 @@
+import ioClient from "socket.io-client";
+
+describe("Socket.io Server", function () {
+  let socket;
+
+  beforeEach(function (done) {
+    socket = ioClient.connect("http://localhost:4000");
+
+    socket.on("connect", function () {
+      done();
+    });
+  });
+});
